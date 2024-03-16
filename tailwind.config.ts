8452eab6b5600d3404decf7ed64ce3585b3c97bb
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2024-03-16 20:34:35 +0000

    feat: initialise Shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
 import type { Config } from "tailwindcss";
 
 const config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
@@ -30,7 +31,24 @@ const config = {
       antiFlashWhite: "#f1f1f1",
       lotion: "#fafafa",
     },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
