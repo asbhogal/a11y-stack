ea18bfa97ad3b670b4eede90fba4aaf64e92dd14
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2024-03-16 15:41:18 +0000

    feat: add metadata to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next.js and TypeScript Boilerplate",
+  description:
+    "A boilerplate for Next.js and TypeScript projects, includes Tailwind, Shadcn, Storybook, React Hook Form, Zod & Husky",
+};
+
 export default function Page() {
   return <h1 className="text-center">Next.js and TypeScript Boilerplate</h1>;
 }
