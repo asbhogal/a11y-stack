b27c3a590d8983cbf36f2d04cbda2aba03b860ac
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2024-03-16 16:09:48 +0000

    chore: config Storybook with Tailwind

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: [
@@ -15,6 +16,19 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/nextjs",
     options: {},
+  },
+  staticDirs: ["../public"],
+  async webpackFinal(config, { configType }) {
+    if (config?.resolve?.alias) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@/lib": path.resolve(__dirname, "../lib"),
+        "@/components": path.resolve(__dirname, "../components"),
+        "@/stories": path.resolve(__dirname, "../stories"),
+        "@/types": path.resolve(__dirname, "../types"),
+      };
+    }
+    return config;
   },
   docs: {
     autodocs: "tag",
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-import "./button.css";
-
 interface ButtonProps {
   /**
    * Is this the principal call to action on the page?
@@ -40,9 +37,13 @@ export const Button = ({
   return (
     <button
       type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " ",
-      )}
+      className={[
+        "bg-black",
+        "text-antiFlashWhite",
+        "storybook-button",
+        `storybook-button--${size}`,
+        mode,
+      ].join(" ")}
       {...props}
     >
       {label}
