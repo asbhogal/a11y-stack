bc286310da2f6c4c5dc17f494f7ca71954b96d27
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2024-03-16 21:30:02 +0000

    feat: add custom components

---
+++ components/globals/Icon.tsx
@@ -0,0 +1,12 @@
+interface IconProps {
+  children: React.ReactNode;
+  svgProps: React.SVGProps<SVGSVGElement>;
+}
+
+export default function Icon({ children, svgProps }: IconProps) {
+  return (
+    <svg aria-hidden focusable={false} {...svgProps} className="icon">
+      {children}
+    </svg>
+  );
+}
