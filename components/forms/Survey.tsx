f48d0b7086468b7604695b575f4e834ec7542da9
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2024-03-24 18:33:10 +0000

    feat: extend survey form components

--- components/forms/Survey.tsx
+++ components/forms/Survey.tsx
@@ -13,6 +13,14 @@ import {
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 import { Button } from "../ui/button";
 import { toast } from "sonner";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Textarea } from "../ui/textarea";
 
 const rating: readonly [
   "very dissatisfied",
@@ -27,14 +35,21 @@ const rating: readonly [
   "satisfied",
   "very satisfied",
 ];
+
+const source = ["website", "newsletter", "job board", "other"];
+
 export default function Survey() {
   const surveyFormSchema = z.object({
+    comments: z.string(),
+    source: z.string(),
     rating: z.enum(rating, { required_error: "Please select a rating" }),
   });
 
   const form = useForm<z.infer<typeof surveyFormSchema>>({
     resolver: zodResolver(surveyFormSchema),
     defaultValues: {
+      comments: "",
+      source: "",
       rating: undefined,
     },
   });
@@ -46,7 +61,44 @@ export default function Survey() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-6"
+      >
+        <FormField
+          control={form.control}
+          name="source"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>How did you hear about us?</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger
+                    className="capitalize"
+                    aria-label="Select a source"
+                  >
+                    <SelectValue placeholder="Select a source" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {source.map((source) => (
+                    <SelectItem
+                      key={source}
+                      value={source}
+                      className="capitalize"
+                    >
+                      {source}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormDescription className="sr-only">
+                Select how you heard about us
+              </FormDescription>
+              <FormMessage className="error-message" />
+            </FormItem>
+          )}
+        />
         <FormField
           control={form.control}
           name="rating"
@@ -64,7 +116,7 @@ export default function Survey() {
                   {rating.map((rating) => (
                     <FormItem
                       key={rating}
-                      className="flex flex-col gap-4 items-center"
+                      className="flex flex-col gap-4items-center"
                     >
                       <FormLabel className="capitalize">{rating}</FormLabel>
                       <FormControl>
@@ -88,7 +140,31 @@ export default function Survey() {
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <FormField
+          control={form.control}
+          name="comments"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>
+                Do you have any comments or suggestions to help us improve our
+                service?
+              </FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Please enter any comments/suggestions"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription className="sr-only">
+                Please enter any comments/suggestions
+              </FormDescription>
+              <FormMessage className="error-message" />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-fit">
+          Submit
+        </Button>
       </form>
     </Form>
   );
