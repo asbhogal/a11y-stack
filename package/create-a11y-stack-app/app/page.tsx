8f8155a794132854c687caa159e79b1bdaca41bc
Aman Bhogal <asbhogal1@live.co.uk>
2024-11-03 15:32:18 +0000

    chore: update package page

--- package/create-a11y-stack-app/app/page.tsx
+++ package/create-a11y-stack-app/app/page.tsx
@@ -1,11 +1,20 @@
 import { Metadata } from "next";
+import React from "react";
 
 export const metadata: Metadata = {
-  title: "a11y-stack | Modular, Extensible, Semantic",
   description:
     "Accessibility-focused, typesafe Next.js frontend framework stack, complete with Storybook, Tailwind CSS, Shadcn, Playwright, React Hook Form, Zod, Prettier, ES Lint and Husky. ",
+  title: "a11y-stack | Modular, Extensible, Semantic",
 };
 
 export default function Page() {
-  return <h1 className="text-center">Welcome to a11y-stack</h1>;
+  return (
+    <React.Fragment>
+      <h1 className="text-center">Welcome to a11y-stack</h1>
+      <h2>
+        To get started, head to page.tsx and start editing content. All styling
+        is in the global.css
+      </h2>
+    </React.Fragment>
+  );
 }
