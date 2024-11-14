c581d92cebfaef34c57a8a4f4a0eaf305a945fb0
Kalon Robson <kalon.robson@innovixx.co.uk>
2024-11-14 17:33:03 +0000

    fix: path mapping on bin app

--- package/create-a11y-stack-app/bin/index.js
+++ package/create-a11y-stack-app/bin/index.js
@@ -9,22 +9,6 @@ const readline = require('readline').createInterface({
   output: process.stdout,
 });
 
-async function copyDirectory(source, destination) {
-  const files = await fs.readdir(source, { withFileTypes: true });
-
-  for (const file of files) {
-    const srcPath = path.join(source, file.name);
-    const destPath = path.join(destination, file.name);
-
-    if (file.isDirectory()) {
-      await fs.mkdir(destPath, { recursive: true });
-      await copyDirectory(srcPath, destPath);
-    } else {
-      await fs.copyFile(srcPath, destPath);
-    }
-  }
-}
-
 async function copyRootFiles(source, destination) {
   const files = await fs.readdir(source, { withFileTypes: true });
 
@@ -33,6 +17,13 @@ async function copyRootFiles(source, destination) {
       const srcPath = path.join(source, file.name);
       const destPath = path.join(destination, file.name);
       await fs.copyFile(srcPath, destPath);
+    } else {
+      fs.mkdir(path.join(destination, file.name), { recursive: true });
+
+      await copyRootFiles(
+        path.join(source, file.name),
+        path.join(destination, file.name),
+      );
     }
   }
 }
@@ -40,43 +31,25 @@ async function copyRootFiles(source, destination) {
 async function createApp(appName) {
   console.log(`Creating ${appName}...`);
 
-  const appDir = path.join(process.cwd(), appName);
+  const appDir = path.isAbsolute(appName)
+    ? path.join(appName)
+    : path.join(process.cwd(), appName);
+
+  try {
+    await fs.access(appDir);
+    throw new Error(`Error: Directory ${appDir} already exists.`);
+  } catch (error) {
+    if (error.code !== 'ENOENT') {
+      throw error;
+    }
+  }
+
   await fs.mkdir(appDir, { recursive: true });
 
-  const scriptDir = path.dirname(__filename);
+  const scriptDir = path.dirname(__dirname);
   const sourceDir = path.resolve(scriptDir, '.');
 
-  console.log(`Source directory: ${sourceDir}`);
-
-  console.log('Contents of source directory:');
-  console.log(await fs.readdir(sourceDir));
-
-  const copyPromises = [
-    copyDirectory(path.join(sourceDir, '.husky'), path.join(appDir, '.husky')),
-    copyDirectory(
-      path.join(sourceDir, '.storybook'),
-      path.join(appDir, '.storybook'),
-    ),
-    copyDirectory(path.join(sourceDir, 'lib'), path.join(appDir, 'lib')),
-    copyDirectory(path.join(sourceDir, 'app'), path.join(appDir, 'app')),
-    copyDirectory(
-      path.join(sourceDir, 'playwright-report'),
-      path.join(appDir, 'playwright-report'),
-    ),
-    copyDirectory(
-      path.join(sourceDir, 'stories'),
-      path.join(appDir, 'stories'),
-    ),
-    copyDirectory(
-      path.join(sourceDir, 'test-results'),
-      path.join(appDir, 'test-results'),
-    ),
-    copyDirectory(path.join(sourceDir, 'tests'), path.join(appDir, 'tests')),
-  ];
-
   await copyRootFiles(sourceDir, appDir);
-
-  await Promise.all(copyPromises);
 
   console.log('\nInstalling dependencies...');
   process.chdir(appDir);
